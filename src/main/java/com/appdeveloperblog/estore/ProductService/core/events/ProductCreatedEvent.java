db25ddfb0419c9a7e453e0ef7c039a042c2929ee
“신형기” <tkaqkeldk@hanatour.com>
2022-02-25 15:34:00 +0900

    [EventSourcing] event sourcing add

---
+++ src/main/java/com/appdeveloperblog/estore/ProductService/core/events/ProductCreatedEvent.java
@@ -0,0 +1,16 @@
+package com.appdeveloperblog.estore.ProductService.core.events;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
