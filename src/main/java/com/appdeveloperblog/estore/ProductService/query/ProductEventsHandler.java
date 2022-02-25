b15f9c605500f241dc8f4952f4d64dc4fe2849f8
“신형기” <tkaqkeldk@hanatour.com>
2022-02-25 22:06:45 +0900

    [EventHandler] Repository connect

--- src/main/java/com/appdeveloperblog/estore/ProductService/query/ProductEventsHandler.java
+++ src/main/java/com/appdeveloperblog/estore/ProductService/query/ProductEventsHandler.java
@@ -1,13 +1,25 @@
 package com.appdeveloperblog.estore.ProductService.query;
 
+import com.appdeveloperblog.estore.ProductService.core.ProductsRepository;
+import com.appdeveloperblog.estore.ProductService.core.data.ProductEntity;
 import com.appdeveloperblog.estore.ProductService.core.events.ProductCreatedEvent;
+import lombok.AllArgsConstructor;
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
 public class ProductEventsHandler {
 
+    private final ProductsRepository productsRepository;
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
+
+        ProductEntity productEntity = new ProductEntity();
+        BeanUtils.copyProperties(event, productEntity);
+
+        productsRepository.save(productEntity);
     }
 }
