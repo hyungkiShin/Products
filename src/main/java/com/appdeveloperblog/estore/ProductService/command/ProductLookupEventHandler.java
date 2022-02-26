6bcaf8b2de04e0405516b63179a3b20d6bd177ad
hyungkiShin <tkaqkeldk@nate.com>
2022-02-27 01:11:18 +0900

    [생성자 Annotation refatoring]

--- src/main/java/com/appdeveloperblog/estore/ProductService/command/ProductLookupEventHandler.java
+++ src/main/java/com/appdeveloperblog/estore/ProductService/command/ProductLookupEventHandler.java
@@ -1,21 +1,24 @@
 package com.appdeveloperblog.estore.ProductService.command;
 
+import com.appdeveloperblog.estore.ProductService.core.data.ProductLookupEntity;
 import com.appdeveloperblog.estore.ProductService.core.data.ProductLookupRepository;
 import com.appdeveloperblog.estore.ProductService.core.events.ProductCreatedEvent;
-import lombok.AllArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 @ProcessingGroup("product-group")
 public class ProductLookupEventHandler {
 
     private final ProductLookupRepository productLookupRepository;
 
+    public ProductLookupEventHandler(ProductLookupRepository productLookupRepository) {
+        this.productLookupRepository = productLookupRepository;
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
-
+        ProductLookupEntity productLookupEntity = new ProductLookupEntity(event.getProductId(), event.getTitle());
     }
 }
--- src/main/java/com/appdeveloperblog/estore/ProductService/query/ProductEventsHandler.java
+++ src/main/java/com/appdeveloperblog/estore/ProductService/query/ProductEventsHandler.java
@@ -3,20 +3,20 @@ package com.appdeveloperblog.estore.ProductService.query;
 import com.appdeveloperblog.estore.ProductService.core.data.ProductsRepository;
 import com.appdeveloperblog.estore.ProductService.core.data.ProductEntity;
 import com.appdeveloperblog.estore.ProductService.core.events.ProductCreatedEvent;
-import lombok.AllArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 @ProcessingGroup("product-group")
 public class ProductEventsHandler {
 
     private final ProductsRepository productsRepository;
+
+    public ProductEventsHandler(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
 
     @EventHandler
     public void on(ProductCreatedEvent event) {
